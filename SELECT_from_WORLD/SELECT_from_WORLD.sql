ef57046f8f0591ba776c21d85a2044edd057b309
Arupam Sengupta <arupam.sengupta@gmail.com>
2024-02-18 19:42:05 +0530

    SELECT from WORLD SQL no. 4-6

--- SELECT_from_WORLD/SELECT_from_WORLD.sql
+++ SELECT_from_WORLD/SELECT_from_WORLD.sql
@@ -5,3 +5,12 @@ SELECT name FROM world
 
 SELECT name, (gdp/population) FROM world 
     WHERE population>= 200000000
+
+SELECT name, (population / 1000000) as "Population(in millions)" FROM world 
+    WHERE continent='South America'
+
+SELECT name, population FROM world 
+    WHERE name IN ('France','Germany','Italy')
+
+SELECT name FROM world 
+    WHERE name like '%United%'
