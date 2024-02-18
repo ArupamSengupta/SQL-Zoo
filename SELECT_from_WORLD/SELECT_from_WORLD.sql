3ed9e1f468b9393e845feeedacb717a4a8a7104e
Arupam Sengupta <arupam.sengupta@gmail.com>
2024-02-18 20:31:22 +0530

    SELECT from WORLD SQL no. 7-9

--- SELECT_from_WORLD/SELECT_from_WORLD.sql
+++ SELECT_from_WORLD/SELECT_from_WORLD.sql
@@ -14,3 +14,12 @@ SELECT name, population FROM world
 
 SELECT name FROM world 
     WHERE name like '%United%'
+
+SELECT name,population,area FROM world 
+    WHERE area>3000000 OR population>250000000
+
+SELECT name,population,area FROM world 
+    WHERE area>3000000 XOR population>250000000
+
+SELECT name, (population/1000000) as "Population(in millions)", (gdp/1000000000) as "GDP(in billions)"
+    WHERE continent = 'South America'
