17fc54f3e77e20a953d8caf4309939c106563cc2
Arupam Sengupta <arupam.sengupta@gmail.com>
2024-02-21 18:15:57 +0530

    SELECT from NOBEL SQL 1-3.

---
+++ SELECT_from_NOBEL/SELECT_from_NOBEL.sql
@@ -0,0 +1,7 @@
+SELECT yr, subject, winner FROM nobel 
+    WHERE yr = 1950
+
+SELECT winner FROM nobel
+ WHERE yr = 1962 AND subject = 'literature'
+
+SELECT yr, subject WHERE winner='Albert Einstein'
