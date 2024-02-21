0e56d2859c4b3aa97056788102d6e72de239aa14
Arupam Sengupta <arupam.sengupta@gmail.com>
2024-02-21 18:54:36 +0530

    SELECT from NOBEL SQL 4-6

--- SELECT_from_NOBEL/SELECT_from_NOBEL.sql
+++ SELECT_from_NOBEL/SELECT_from_NOBEL.sql
@@ -2,6 +2,18 @@ SELECT yr, subject, winner FROM nobel
     WHERE yr = 1950
 
 SELECT winner FROM nobel
- WHERE yr = 1962 AND subject = 'literature'
+    WHERE yr = 1962 AND subject = 'literature'
 
 SELECT yr, subject WHERE winner='Albert Einstein'
+
+SELECT winner FROM nobel 
+    WHERE yr >= 2000 AND subject='Peace'
+
+SELECT yr, subject, winner FROM nobel 
+    WHERE subject= 'Literature' AND yr BETWEEN 1980 TO 1989
+
+SELECT * FROM nobel
+    WHERE yr = 1970 AND winner IN ('Theodore Roosevelt',
+    'Thomas Woodrow Wilson',
+    'Jimmy Carter',
+    'Barack Obama')
